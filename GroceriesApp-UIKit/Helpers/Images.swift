c96e91988393b0c3f206025b0602988c1312066d
Mutlu Çalkan <mutlu.calkan@gmail.com>
2024-01-12 10:02:44 +0300

    Project folders created.

---
+++ GroceriesApp-UIKit/Helpers/Images.swift
@@ -0,0 +1,26 @@
+//
+//  Images.swift
+//  GroceriesApp-UIKit
+//
+//  Created by Mutlu Çalkan on 12.01.2024.
+//
+
+import UIKit
+
+struct Images {
+    
+    struct Welcome {
+        static let logo = UIImage(named: "logo")!
+        static let background = UIImage(named: "welcomeBackground")!
+    }
+    
+    struct Category {
+        static let bakery = UIImage(named: "bakery")!
+        static let beverages = UIImage(named: "beverages")!
+        static let dairyEggs = UIImage(named: "dairyEggs")!
+        static let fruitsVegs = UIImage(named: "fruitsVegs")!
+        static let meatFish = UIImage(named: "meatFish")!
+        static let oils = UIImage(named: "oils")!
+    }
+    
+}
---
+++ GroceriesApp-UIKit/View/Controller/WelcomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WelcomeViewController.swift
+//  GroceriesApp-UIKit
+//
+//  Created by Mutlu Çalkan on 12.01.2024.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        }
+    
+
+}
